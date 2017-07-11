91570732da6d22fb7ea11627b631a278308c4e81
Alejandro Sanchez <aalejo@gmail.com>
2017-07-11 21:07:30 +0000

    inserting the _tk child file

---
+++ wp-content/themes/_tk-child/functions.php
@@ -0,0 +1,34 @@
+<?php
+
+    /**
+     * Autoload for PHP Composer
+     */
+    require "vendor/autoload.php";
+
+    /**
+     * Here we are importing the Styles of the parent theme and re-using them
+     * for our own project, please don't edit this hook/function
+     */
+    add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
+    function my_theme_enqueue_styles() {
+        wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    
+    }
+
+    /**
+     * This is an example of usage for the Eloquent ORM
+
+        $db = \WeDevs\ORM\Eloquent\Database::instance();
+        
+        var_dump( $db->table('users')->find(1) );
+        var_dump( $db->select('SELECT * FROM wp_users WHERE id = ?', [1]) );
+        var_dump( $db->table('users')->where('user_login', 'john')->first() );
+    */
+    
+    /** 
+     * Start your own functions here
+     */
+     
+     
+     
+?>
